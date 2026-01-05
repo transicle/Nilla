e758e6b251ff32b5dc4b0e5713f425dddeb4fc06
lily <ilovesuno@proton.me>
2026-01-04 23:18:15 -0700

    Create Install-Dependencies.sh for Nilla dependencies
    This script installs necessary dependencies for Nilla, including applications like nautilus, rofi, discord, and kitty.

---
+++ Install-Dependencies.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+#  Install all of the dependencies that Nilla depends on,
+#  including the applications used.
+#
+
+sudo pacman -Syu --noconfirm # Update packages incase of updates.
+sudo pacman -Sy nautilus \
+               rofi \
+               discord \
+               kitty --noconfirm
