fa32a5884d0e6b87e9c5c216db64518795e44e10
lily <ilovesuno@proton.me>
2026-01-13 22:00:04 -0700

    Add files via upload

--- Install-Dependencies.sh
+++ Install-Dependencies.sh
@@ -6,11 +6,11 @@
 
 sudo pacman -Syu --noconfirm # Update packages incase of updates.
 sudo pacman -S nautilus \
-                rofi \
-                discord \
-                kitty \
-                git \
-                dunst \
-                flatpak --noconfirm
+		rofi \
+		discord \
+		kitty \
+		git \
+		dunst \
+		flatpak --noconfirm
 
 flatpak update -y
