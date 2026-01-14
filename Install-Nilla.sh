a30e6d2ba3a71d51a92871610e7d2ca50d9ac69b
lily <ilovesuno@proton.me>
2026-01-13 21:29:59 -0700

    Update Install-Nilla.sh to clone and set up Nilla
    Clone Nilla repository and install dependencies.

--- Install-Nilla.sh
+++ Install-Nilla.sh
@@ -4,6 +4,15 @@
 #  from the GitHub repository.
 #
 
+NILLA_REPO="git@github.com:transicle/Nilla"
+
+cd $HOME
+mkdir NILLA_TEMP
+cd NILLA_TEMP
+
+git clone $NILLA_REPO
+
+cd Nilla
 chmod +x ./Install-Dependencies.sh
 ./Install-Dependencies.sh
 
@@ -12,5 +21,8 @@ cp -r ./hypr $HOME/.config/
 
 rm -rf $HOME/.config/dunst
 cp -r ./dunst $HOME/.config/
+
+cd $HOME
+rm -rf NILLA_TEMP
 
 hyprctl reload
