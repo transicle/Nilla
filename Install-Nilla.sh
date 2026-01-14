46abcbaaa1780bac4fcee9b74d0d10ace1ef5d7d
lily <ilovesuno@proton.me>
2026-01-13 21:10:25 -0700

    Change Nilla repo URL to SSH and update file handling

--- Install-Nilla.sh
+++ Install-Nilla.sh
@@ -4,16 +4,13 @@
 #  from the GitHub repository.
 #
 
-NILLA_REPO="https://github.com/transicle/Nilla.git"
+NILLA_REPO="git@github.com:transicle/Nilla.git"
+
 chmod +x ./Install-Dependencies.sh
 ./Install-Dependencies.sh
-cd $HOME
-mkdir Nilla
-cd Nilla
-git clone $NILLA_REPO
+
 rm -rf $HOME/.config/hypr
-mv ./hypr $HOME/.config/
+cp ./hypr $HOME/.config/
+
 rm -rf $HOME/.config/dunst
-mv ./dunst $HOME/.config/
-cd ..
-rm -rf Nilla
+cp ./dunst $HOME/.config/
