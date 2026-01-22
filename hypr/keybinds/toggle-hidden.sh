ce5b2b1f3a64c6ca83f56082ddd6f839c519b10c
lily <ilovesuno@proton.me>
2026-01-21 19:35:42 -0700

    Create toggle-hidden.sh for workspace management

---
+++ hypr/keybinds/toggle-hidden.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+ADDRESS=$(hyprctl -j activewindow |jq -r '.address')
+WORKSPACE=$(hyprctl -j activewindow | jq -r '.workspace.name')
+
+if [[ "$WORKSPACE" == "special:hidden" ]]; then
+	CURRENT_WORKSPACE=$(hyprctl -j activeworkspace | jq -r '.id')
+	hyprctl dispatch movetoworkspace "$CURRENT_WORKSPACE,address:$ADDRESS"
+else
+	hyprctl dispatch movetoworkspace "special:hiden,address:$ADDRESS"
+fi
